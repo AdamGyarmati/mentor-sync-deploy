8f1808d8480a0e3ee98d422f18a7e86d02411540
gyarmatiadam <gyari12@gmail.com>
2024-01-31 09:29:49 +0100

    Add existing project files to Git

---
+++ security-demo-with-angular/src/main/java/hu/gyarmati/securitydemowithangular/config/SpringWebSecurity.java
@@ -0,0 +1,41 @@
+package hu.gyarmati.securitydemowithangular.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan("hu.gyarmati")
+public class SpringWebSecurity implements WebMvcConfigurer {
+
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource
+                = new ReloadableResourceBundleMessageSource();
+
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://ec2-3-70-135-81.eu-central-1.compute.amazonaws.com",
+                        "http://localhost:4200",
+                        "http://adamgyarmati.com",
+                        "adamgyarmati.com",
+                        "https://adamgyarmati.com",
+                        "https://www.adamgyarmati.com")
+                .allowedMethods("GET", "POST", "DELETE", "PUT", "OPTIONS")
+                .allowedHeaders("*");
+    }
+}
